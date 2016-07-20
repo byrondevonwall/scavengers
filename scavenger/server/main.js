174fe7f4cba318f781d71fe202d756c75f2f4adc
Byron Wall <byrondevonwall@gmail.com>
2016-07-20 10:17:25 -0400

    move app.js and routes.js to proper place, create users with team roles

--- scavenger/server/main.js
+++ scavenger/server/main.js
@@ -1,5 +1,33 @@
-import { Meteor } from 'meteor/meteor';
+if(Meteor.isServer){
+  var users = [
+    {name: "Matt", email:"matt@matt.com", roles: ['team-1']},
+    {name: "Damian", email:"damian@damian.com", roles: ['team-1']},
+    {name: "Edd", email:"edd@edd.com", roles: ['team-1']},
+    {name: "Daniel", email: "daniel@daniel.com", roles: ['team-1']},
+    {name: "Mike", email: "mike@mike.com", roles: ['team-1']},
+    {name: "Bob", email: "bob@bob.com", roles: ['team-2']},
+    {name: "Jim", email: "jim@jim.com", roles: ['team-2']},
+    {name: "Molly", email: "molly@molly.com", roles: ['team-2']},
+    {name: "John", email: "john@john.com", roles: ['team-2']},
+    {name: "Jeremy", email: "jeremy@jeremy.com", roles: ['team-2']},
+    {name: "Chessa", email: "chessa@chessa.com", roles: ['team-3']},
+    {name: "Karin", email: "karin@karin.com", roles: ['team-3']},
+    {name: "Allie", email: "allie@allie.com", roles: ['team-3']},
+    {name: "Jesalyn", email: "jesalyn@jesalyn.com", roles: ['team-3']},
+    {name: "Katie", email: "katie@katie.com", roles: ['team-3']}
+  ];
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+  // _.each(users, function(user){
+  //   var id;
+  //
+  //   id = Accounts.createUser({
+  //     email: user.email,
+  //     password: "password",
+  //     profile: {name: user.name}
+  //   });
+  //
+  //   if(user.roles.length > 0){
+  //     Roles.addUsersToRoles(id, user.roles, 'default-group')
+  //   }
+  // });//end each
+}//end isServer
