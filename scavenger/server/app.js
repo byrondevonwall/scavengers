30239ac5210a44d5d7aa37fee0e622b0894e1152
Byron Wall <byrondevonwall@gmail.com>
2016-09-05 12:56:41 -0400

    heroku time

--- scavenger/server/app.js
+++ scavenger/server/app.js
@@ -17,45 +17,6 @@ if(Meteor.isServer){
    }
 
 
-//----------team/question creation arrays----------//
-
-  //declare seed users
-  var users = [
-    {name: "Matt", email:"matt@matt.com", roles: ['team-1']},
-    {name: "Damian", email:"damian@damian.com", roles: ['team-1']},
-    {name: "Edd", email:"edd@edd.com", roles: ['team-1']},
-    {name: "Daniel", email: "daniel@daniel.com", roles: ['team-1']},
-    {name: "Mike", email: "mike@mike.com", roles: ['team-1']},
-    {name: "Bob", email: "bob@bob.com", roles: ['team-2']},
-    {name: "Jim", email: "jim@jim.com", roles: ['team-2']},
-    {name: "Molly", email: "molly@molly.com", roles: ['team-2']},
-    {name: "John", email: "john@john.com", roles: ['team-2']},
-    {name: "Jeremy", email: "jeremy@jeremy.com", roles: ['team-2']},
-    {name: "Chessa", email: "chessa@chessa.com", roles: ['team-3']},
-    {name: "Karin", email: "karin@karin.com", roles: ['team-3']},
-    {name: "Allie", email: "allie@allie.com", roles: ['team-3']},
-    {name: "Jesalyn", email: "jesalyn@jesalyn.com", roles: ['team-3']},
-    {name: "Katie", email: "katie@katie.com", roles: ['team-3']}
-  ];
-
-  // Meteor.call('createQuestion', true, false, false, true, '0', '0', '', false, 'What is your name?','',false,'',0,'team-1', 1)
-  // isSA, isItem, isPic, picUrl, hasItem, questionText, shortAnswer, isAnswered, answerTime, ptsAwarded, groupName, questionNumber)
-
-  // when we drop the database, this block of code is how we add users
-    // _.each(users, function(user){
-    //   var id;
-    //
-    //   id = Accounts.createUser({
-    //     email: user.email,
-    //     password: "password",
-    //     profile: {name: user.name}
-    //   });
-    //
-    //   if(user.roles.length > 0){
-    //     Roles.addUsersToRoles(id, user.roles, 'defaultGroup')
-    //   }
-    // });//end each
-
 //this code uploads photos
 
   S3.config = {
@@ -64,33 +25,6 @@ if(Meteor.isServer){
     bucket: 'scavengerhuntphotos'
   }
 
-
-
-  //   Slingshot.fileRestrictions("uploadFiles", {
-  //     allowedFileTypes: ["image/png", "image/jpeg", "image/gif"],
-  //     maxSize: 10 * 1024 * 1024 // 10 MB (use null for unlimited)
-  // });
-  //
-  //   Slingshot.createDirective("uploadFiles", Slingshot.S3Storage, {
-  //     bucket: "scavengerhuntphotos",
-  //     acl: "public-read",
-  //
-  //
-  //     authorize: function(){
-  //       //you can't upload if youre not logged in
-  //       if(!this.userId){
-  //         var message = "Please log in before posting files";
-  //         throw new Meteor.Error("Login Required", message);
-  //       }
-  //       return true;
-  //     },
-  //
-  //     key: function(file){
-  //       //store file in a directory based on a users team name
-  //       var teamName = Meteor.user().roles.defaultGroup[0]
-  //       return teamName + "/" + file.name;
-  //     }
-  //   });
 
 };//end isServer
 
