04ab2fae6e47c51594d2104959482a7bdb3ea501
Byron Wall <byrondevonwall@gmail.com>
2016-09-04 22:21:27 -0400

    access rules

--- scavenger/mobile-config.js
+++ scavenger/mobile-config.js
@@ -9,12 +9,12 @@ App.info({
 });
 
 
-// App.accessRule('https://cc-scavenger-hunt.herokuapp.com/*');
-// App.accessRule('http://cc-scavenger-hunt.herokuapp.com/*');
-// App.accessRule('https://s3.amazonaws.com/scavengerhuntphotos/*');
-// App.accessRule('http://localhost:3010/*');
+App.accessRule('https://cc-scavenger-hunt.herokuapp.com/*');
+App.accessRule('http://cc-scavenger-hunt.herokuapp.com/*');
+App.accessRule('https://s3.amazonaws.com/scavengerhuntphotos/*');
+App.accessRule('http://localhost:3010/*');
 App.accessRule("blob:*");
-App.accessRule("*")
+// App.accessRule("*")
 
 
 App.icons({'iphone_2x': 'public/images/mobile/icons/2016CSHlogo-iphone_2x.png',
